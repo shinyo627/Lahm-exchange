e0435135c614ffbd4c60e3590323296c6598c5cc
shinyo627 <shinyo627@gmail.com>
2021-02-13 14:44:33 +0100

    Basic client setup, created few components and styling for MenuItem.js

---
+++ src/pages/homepage/Homepage.js
@@ -0,0 +1,13 @@
+import Directory from '../../components/directory/Directory';
+
+import './HomepageStyle.scss';
+
+const Homepage = (prop) => {
+  return (
+    <div className='homepage'>
+      <Directory />
+    </div>
+  );
+};
+
+export default Homepage;
