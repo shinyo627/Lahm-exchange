de234ac496989c74a1cd51adb252ada663068cd0
shinyo627 <shinyo627@gmail.com>
2021-02-26 12:35:29 +0100

    CollectionItem components now renders missing dollar sign $ on UI

--- src/components/collection-item/CollectionItem.js
+++ src/components/collection-item/CollectionItem.js
@@ -20,7 +20,7 @@ const CollectionItem = ({ item, addItem }) => {
       />
       <div className='collection-footer'>
         <span className='name'>{name}</span>
-        <span className='name'>{price}</span>
+        <span className='name'>${price}</span>
       </div>
       <CustomButton onClick={() => addItem(item)} inverted>
         ADD TO CART
